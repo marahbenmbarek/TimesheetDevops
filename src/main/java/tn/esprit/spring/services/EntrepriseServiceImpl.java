53650369642c767b7be578584f44919ffef4fee8
ons <jawadi.ons.97@gmail.com>
2021-11-06 17:28:10 +0100

    sonartestconfig

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -31,11 +31,7 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	}
 	
 	public void affecterDepartementAEntreprise(int depId, int entrepriseId) {
-		//Le bout Master de cette relation N:1 est departement  
-				//donc il faut rajouter l'entreprise a departement 
-				// ==> c'est l'objet departement(le master) qui va mettre a jour l'association
-				//Rappel : la classe qui contient mappedBy represente le bout Slave
-				//Rappel : Dans une relation oneToMany le mappedBy doit etre du cote one.
+
 				Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
 				Departement depManagedEntity = deptRepoistory.findById(depId).get();
 				
@@ -46,6 +42,7 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	
 	public List<String> getAllDepartementsNamesByEntreprise(int entrepriseId) {
 		Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
+		
 		List<String> depNames = new ArrayList<>();
 		for(Departement dep : entrepriseManagedEntity.getDepartements()){
 			depNames.add(dep.getName());
--- src/test/java/tn/esprit/spring/EntrepriseTest.java
+++ src/test/java/tn/esprit/spring/EntrepriseTest.java
@@ -6,22 +6,46 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
+
+
+
 import tn.esprit.spring.entities.Departement;
+
 import tn.esprit.spring.entities.Entreprise;
+
+import tn.esprit.spring.repository.DepartementRepository;
+import tn.esprit.spring.repository.EntrepriseRepository;
+
 import tn.esprit.spring.services.EntrepriseServiceImpl;
+import tn.esprit.spring.services.IDepartementService;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class EntrepriseTest {
-	@Autowired EntrepriseServiceImpl Entreprisee ;
+	  
+
+	Logger logger = LoggerFactory.getLogger(EntrepriseTest.class);
 	
 	
-	public EntrepriseTest(){
-		
-	}
+	
+	@Autowired 
+	EntrepriseServiceImpl entrepriseeService ;
+	
+	@Autowired
+	IDepartementService idepartement;
+	
+	@Autowired
+	DepartementRepository idepartementrepo;
+	
+	@Autowired
+	EntrepriseRepository ent;
+	
 	
 	
 	@Test
@@ -30,12 +54,60 @@ public class EntrepriseTest {
 		Entreprise entreprise = new Entreprise() ;
 		entreprise.setName("IPACT");
 		entreprise.setRaisonSocial("azerty");
-		List<Departement> list = new ArrayList();
+		List<Departement> list = new ArrayList<>();
 		list.add(new Departement(1,"physique"));
 	
-		//entreprise.setDepartements(list);
-		Entreprisee.ajouterEntreprise(entreprise);
+	
+		entrepriseeService.ajouterEntreprise(entreprise);
 	}
+
+	@Test
+	public void testAjouterDepartement(){
+		
+		
+		
+		 Departement departement = new Departement();
+	
+		 
+		    departement.setName("physique");
+		    departement.setId(1);
+	    
+		    idepartement.ajouterDepartement(departement);
+
+	       
+	       logger.info("l'employé est ajouté");
+		}
+	
+
+	@Test
+	public void  testDeleteDepartementById(){
+		Departement department = new Departement("dev");
+		int d = entrepriseeService.ajouterDepartement(department);
+		entrepriseeService.deleteDepartementById(d);
+		idepartementrepo.findById(d);
+		
+		logger.info("success!! " );
+		
+		
+	}
+	
+	@Test
+	public void testgetEntrepriseById(){
+		
+		Entreprise entre =new Entreprise("lait", "bbbbbb") ;
+	int	id=entrepriseeService.ajouterEntreprise(entre);
+		
+		
+		logger.info("l objet n est pas null " );
+		entrepriseeService.getEntrepriseById(id);
+		
+	
+		logger.info("done!!" );
+	}
+	
+	
+	
+	
 	
 
 }
